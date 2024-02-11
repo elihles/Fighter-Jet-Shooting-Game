0f05147fab8d6b8bdc14d2d628baf4cd6a38201b
elihl <elihl@LAPTOP-EVR5KTER>
2024-02-11 10:52:30 +0200

    Add project files.

---
+++ Fighter Jet Shooting Game/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Fighter_Jet_Shooting_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.bullet = new System.Windows.Forms.PictureBox();
+            this.enemyThree = new System.Windows.Forms.PictureBox();
+            this.enemyTwo = new System.Windows.Forms.PictureBox();
+            this.enemyOne = new System.Windows.Forms.PictureBox();
+            this.txtScore = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bullet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemyThree)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemyTwo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemyOne)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player
+            // 
+            this.player.Image = global::Fighter_Jet_Shooting_Game.Properties.Resources.player;
+            this.player.Location = new System.Drawing.Point(366, 635);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(110, 98);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.player.TabIndex = 4;
+            this.player.TabStop = false;
+            // 
+            // bullet
+            // 
+            this.bullet.Image = global::Fighter_Jet_Shooting_Game.Properties.Resources.bullet;
+            this.bullet.Location = new System.Drawing.Point(366, 287);
+            this.bullet.Name = "bullet";
+            this.bullet.Size = new System.Drawing.Size(7, 27);
+            this.bullet.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.bullet.TabIndex = 3;
+            this.bullet.TabStop = false;
+            // 
+            // enemyThree
+            // 
+            this.enemyThree.Image = global::Fighter_Jet_Shooting_Game.Properties.Resources.enemy2;
+            this.enemyThree.Location = new System.Drawing.Point(632, 12);
+            this.enemyThree.Name = "enemyThree";
+            this.enemyThree.Size = new System.Drawing.Size(100, 85);
+            this.enemyThree.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.enemyThree.TabIndex = 2;
+            this.enemyThree.TabStop = false;
+            // 
+            // enemyTwo
+            // 
+            this.enemyTwo.Image = global::Fighter_Jet_Shooting_Game.Properties.Resources.enemy1;
+            this.enemyTwo.Location = new System.Drawing.Point(341, 12);
+            this.enemyTwo.Name = "enemyTwo";
+            this.enemyTwo.Size = new System.Drawing.Size(100, 85);
+            this.enemyTwo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.enemyTwo.TabIndex = 1;
+            this.enemyTwo.TabStop = false;
+            // 
+            // enemyOne
+            // 
+            this.enemyOne.Image = global::Fighter_Jet_Shooting_Game.Properties.Resources.enemy;
+            this.enemyOne.Location = new System.Drawing.Point(28, 14);
+            this.enemyOne.Name = "enemyOne";
+            this.enemyOne.Size = new System.Drawing.Size(100, 85);
+            this.enemyOne.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.enemyOne.TabIndex = 0;
+            this.enemyOne.TabStop = false;
+            // 
+            // txtScore
+            // 
+            this.txtScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtScore.Location = new System.Drawing.Point(2, 340);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(800, 128);
+            this.txtScore.TabIndex = 5;
+            this.txtScore.Text = "0";
+            this.txtScore.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.MainGameTimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(807, 745);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.bullet);
+            this.Controls.Add(this.enemyTwo);
+            this.Controls.Add(this.enemyOne);
+            this.Controls.Add(this.enemyThree);
+            this.Name = "Form1";
+            this.Text = "Fighting Jet Shooting GAME";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisUP);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bullet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemyThree)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemyTwo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemyOne)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox enemyOne;
+        private System.Windows.Forms.PictureBox enemyTwo;
+        private System.Windows.Forms.PictureBox enemyThree;
+        private System.Windows.Forms.PictureBox bullet;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
